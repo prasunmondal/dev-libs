cf112e848280e891bc07366b99f8852a19ccfbf1
prasunmondal <prasun.mondal02@gmail.com>
2024-06-17 21:03:08 +0530

    Update build.gradle.kts

--- libs/build.gradle.kts
+++ libs/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 //    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 //    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.android.volley:volley:1.2.1")
 }
