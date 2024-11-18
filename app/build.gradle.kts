18416c9617ce3f6ee3d57c62f540f1f3aa2cc903
Prasun Mondal <prasun.mondal02@gmail.com>
2024-11-18 16:42:45 +0530

    targetSdk = 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.prasunmondal.dev.libs"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.prasunmondal.dev.libs"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
