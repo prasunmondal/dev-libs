0ce1feb447ad148f7b36bb8b126c769a1cb331e0
Prasun Mondal <prasun.mondal02@gmail.com>
2024-11-20 21:51:12 +0530

    update scriptURL

--- app/src/main/java/com/prasunmondal/dev/libs/gsheet/operations/DeploymentID.kt
+++ app/src/main/java/com/prasunmondal/dev/libs/gsheet/operations/DeploymentID.kt
@@ -2,7 +2,41 @@ package com.prasunmondal.dev.libs.gsheet.operations
 
 class DeploymentID {
     companion object {
+        /*
+
+        Previous Release Deployments
+        ---------------------------------
+
+        AKfycby0rORN4PWBqs2XR2JFTlMHBA2kphA7A8LLrAQ40yF8aiXveEa1cqUJA-glH36VdRZ0Pg
+
+
+
+
+        Delete all release except previous:
+        ------------------------------------
+
+        # List of deployment IDs to protect
+        PROTECTED_IDS=("AKfycby0rORN4PWBqs2XR2JFTlMHBA2kphA7A8LLrAQ40yF8aiXveEa1cqUJA-glH36VdRZ0Pg"
+                       "AKfycbws8KPXUkLBmDnRm2g3_z6V5rOrTDgbS9W9m6Um7k5norXL8SdPFZiYDMxqsqHumK0aaA")
+
+
+        # Fetch all deployments and process each deployment ID
+        clasp deployments | grep 'AKfycb' | awk '{print $2}' | while read id; do
+            # Check if the ID is in the protected list
+            if [[ " ${PROTECTED_IDS[@]} " =~ " ${id} " ]]; then
+                echo "Skipping deployment ID: $id (Protected)"
+            else
+                echo "Deleting deployment ID: $id"  # Log the ID being deleted
+                clasp undeploy $id
+            fi
+        done
+
+
+         */
+
+
+
         var deploymentId =
-            "AKfycby0rORN4PWBqs2XR2JFTlMHBA2kphA7A8LLrAQ40yF8aiXveEa1cqUJA-glH36VdRZ0Pg"
+            "AKfycbws8KPXUkLBmDnRm2g3_z6V5rOrTDgbS9W9m6Um7k5norXL8SdPFZiYDMxqsqHumK0aaA"
     }
 }
